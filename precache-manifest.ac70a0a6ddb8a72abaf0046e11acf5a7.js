7062ca85f8c61f2be046b6d7bcb98d89ce5620cf
BirdyAllan <alexsimion998@gmail.com>
2020-08-01 23:19:51 +0300

    Updates

---
+++ precache-manifest.ac70a0a6ddb8a72abaf0046e11acf5a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39237cb9df7694a32292993760f2feb6",
+    "url": "/TheCouncil/index.html"
+  },
+  {
+    "revision": "0ecae0f84f7f265caae7",
+    "url": "/TheCouncil/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "560ce3ebf6e95466acee",
+    "url": "/TheCouncil/static/css/main.776908a3.chunk.css"
+  },
+  {
+    "revision": "0ecae0f84f7f265caae7",
+    "url": "/TheCouncil/static/js/2.4136a392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheCouncil/static/js/2.4136a392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560ce3ebf6e95466acee",
+    "url": "/TheCouncil/static/js/main.29b98824.chunk.js"
+  },
+  {
+    "revision": "4e392d6f8a4f16c74ec1",
+    "url": "/TheCouncil/static/js/runtime-main.04c3fd8a.js"
+  },
+  {
+    "revision": "69baeda7e6d0edcab803a0bb4243db3c",
+    "url": "/TheCouncil/static/media/MavisBold-owXzz.69baeda7.woff"
+  }
+]);
